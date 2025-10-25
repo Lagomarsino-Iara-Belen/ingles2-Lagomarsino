30a936db983478faa46db42be7ef8d0edc813913
Lagomarsino, Iara Belén <lagomarsinoiara@gmail.com>
2025-10-24 21:31:39 -0300

    Creación del data  NExp
    Propósito:
    Se crea el data  NExp. Se añade ejemplos de como se crea al tipo y poder utilizar como test.
    Además del data y los ejemplos, se le agrega una función que permite simplificar el tipo creado.
    
    Test: 
    Los 3 ejemplos dados sirven como ejemplos para corroborar que la función me de el resultado esperado.  
    
    comentarios: 
    @Lagomarsino-Iara: Los ejemplos dan el resultado esperado. la función "evalNBinOp" podría mejorarse.

---
+++ PRdeIngles2.hs
@@ -0,0 +1,41 @@
+data NExp = Var Variable
+          | NCte Int
+          | NBOp NBinOp NExp NExp
+    deriving Show 
+
+data NBinOp = Add | Sub | Mul | Div | Mod | Pow
+    deriving Show
+
+type Variable = String
+
+--Ejemplos de como funciona el tipo
+nexp1 = NBOp Add (NCte 5)
+                 (Var "x")
+
+nexp2 = NBOp Add (NCte 5)
+                 (NCte 6) 
+
+nexp3 = NBOp Add (NCte 5)
+                 ( NBOp Mul (Var "x")  
+                            (Var "y"))              
+
+
+
+--cfNE :: NExp -> NExp ==> Devuelve una NExp sin expresiones optimizables 
+-- Nota: le doy una expresion y la devuelve toda optimizada (simplificada)
+cfNE :: NExp -> NExp
+cfNE (Var st)           = (Var st)
+cfNE (NCte n)           = (NCte n)
+cfNE (NBOp bop ne1 ne2) = reducir bop (cfNE ne1) (cfNE ne2)
+
+reducir :: NBinOp -> NExp -> NExp -> NExp
+reducir bop (NCte n) (NCte m) = NCte (evalNBinOp bop n m)
+reducir bop  ex1      ex2     = NBOp bop ex1 ex2
+
+evalNBinOp :: NBinOp -> Int -> Int -> Int
+evalNBinOp Add e1 e2 = (+) e1 e2
+evalNBinOp Sub e1 e2 = (-) e1 e2
+evalNBinOp Mul e1 e2 = (*) e1 e2
+evalNBinOp Div e1 e2 = div e1 e2
+evalNBinOp Mod e1 e2 = mod e1 e2
+evalNBinOp Pow e1 e2 = (^) e1 e2
